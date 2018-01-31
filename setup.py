d206a19d05bdf8c2a58b4fe8db1c0ad982b86506
Max Bolingbroke <batterseapower@hotmail.com>
2018-01-31 18:45:13 +0000

    Make installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bitfinexpy',
+      version='1.0',
+      url='https://github.com/jimako1989/bitfinexpy',
+      packages=['bitfinexpy'],
+     )
